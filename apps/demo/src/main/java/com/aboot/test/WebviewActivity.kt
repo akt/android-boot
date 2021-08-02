7e807ff7d0797637b363654077b1224fd279f950
ak <altair.ke@gmail.com>
2021-08-02 17:39:01 +0800

    test webview

--- apps/demo/src/main/java/com/aboot/test/WebviewActivity.kt
+++ apps/demo/src/main/java/com/aboot/test/WebviewActivity.kt
@@ -1,8 +1,12 @@
 package com.aboot.test
 
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import com.aboot.R
+import com.aboot.test.Base64.getEncoder
+import com.aboot.test.Base64.getUrlDecoder
+import com.aboot.test.Base64.getUrlEncoder
 import com.google.firebase.remoteconfig.FirebaseRemoteConfig
 import kotlinx.android.synthetic.main.activity_webview.*
 
@@ -18,11 +22,20 @@ class WebviewActivity: AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_webview)
 
+        Log.d("xxx1", getUrlEncoder().encodeToString(half.toByteArray()))
+        Log.d("xxx2", getUrlEncoder().encodeToString(one.toByteArray()))
+        Log.d("xxx3", getUrlEncoder().encodeToString(two.toByteArray()))
+
         wb.loadUrl("file:///android_asset/x.html");
 
 
     }
 
+    companion object {
+        const val half = "https://www.binance.com/en/my/settings/profile?returnTo=NATIVE&kycBizMode=BASIC&kyc_bizScene=OCBS"
+        const val one = "https://www.binance.com/en/my/settings/profile?returnTo=NATIVE&kycBizMode=ADVANCE&kyc_bizScene=OCBS"
+        const val two = "https://www.binance.com/en/my/settings/profile/address-verification?returnTo=NATIVE&kyc_bizScene=OCBS"
+    }
 
 
 }
